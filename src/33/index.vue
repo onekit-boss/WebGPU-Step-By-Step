11134232931feeef3aee2e80601f2298cc18908b
OneKit <onekit@onekit.cn>
2023-03-03 17:52:44 +0800

    .

--- src/33/index.vue
+++ src/33/index.vue
@@ -1,6 +1,34 @@
 <template>
-
-<div>
-   </div>
-
+   <canvas id="canvas-webgpu"></canvas>
 </template>
+<script>
+import { CreateShapeWithTexture } from './texture';
+import { CubeData } from '../vertex_data';
+import $ from 'jquery';
+
+export default {
+   mounted() {
+
+      const CreateShape = async (ul, vl, li, textureFile,
+         addressModeU, addressModeV, isAnimation) => {
+         const data = CubeData(ul, vl);
+         await CreateShapeWithTexture(data.positions, data.normals, data.uvs, textureFile, addressModeU, addressModeV, li, isAnimation);
+      }
+
+      let textureFile = 'brick.png';
+      let addressModeU = 'repeat';
+      let addressModeV = 'repeat';
+      let li = {};
+      let isAnimation = true;
+      let ul = 1;
+      let vl = 1;
+      CreateShape(ul, vl, li, textureFile, addressModeU, addressModeV, isAnimation);
+
+   }
+}
+</script>
+<style>
+body {
+   margin: 0
+}
+</style>
